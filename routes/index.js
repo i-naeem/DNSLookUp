bbe76f09080924b5b80ceb771f3ac0b3a461e362
i-naeem <inaeem@protonmail.com>
2022-05-19 20:09:31 +0500

    :sparkles: Setup lookup and reverse route

--- routes/index.js
+++ routes/index.js
@@ -1,20 +1,31 @@
-const DNSLoopUp = require('../services/DNSLookUp');
 const express = require('express');
+const dns = require('dns/promises');
 
 const router = express.Router();
 
-router.get('/', async (req, res, next) => {
+router.get('/lookup', async (req, res, next) => {
   const { domain } = req.query;
-
   if (!domain) {
     return res.status(422).json({
-      type: 'error',
-      message: 'Missing required query param',
+      message: 'Missing domain query param',
     });
   }
-
   try {
-    return res.json(await DNSLoopUp.lookUp(domain));
+    return res.json(await dns.lookup(domain, { all: true }));
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.get('/reverse', async (req, res, next) => {
+  const { address } = req.query;
+  if (!address) {
+    return res.status(422).json({
+      message: 'Missing address query param',
+    });
+  }
+  try {
+    return res.json(await dns.reverse(address, { all: true }));
   } catch (error) {
     next(error);
   }
--- services/DNSLookUp.js
+++
@@ -1,7 +0,0 @@
-const dns = require('dns');
-const util = require('util');
-
-const lookUp = util.promisify(dns.lookup);
-const reverse = util.promisify(dns.reverse);
-
-module.exports = { lookUp, reverse };
